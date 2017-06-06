bf154e3eaab08c478435019907f9c2e3a2e118e7
Fofight <2810037137@qq.com>
2017-06-06 02:21:05 +0000

    Add files via upload

---
+++ LearnByGame/keyboardrules.py
@@ -0,0 +1,555 @@
+#
+#this is the keyboard rules
+#
+
+def pygameKeyboardRule_KEYDOWN():
+        
+	if event.type == K_0:
+		pass
+	if event.type == K_1:
+		pass
+	if event.type == K_2:
+		pass
+	if event.type == K_3:
+		pass
+	if event.type == K_4:
+		pass
+	if event.type == K_5:
+		pass
+	if event.type == K_6:
+		pass
+	if event.type == K_7:
+		pass
+	if event.type == K_8:
+		pass
+	if event.type == K_9:
+		pass
+	if event.type == K_AMPERSAND:
+		pass
+	if event.type == K_ASTERISK:
+		pass
+	if event.type == K_AT:
+		pass
+	if event.type == K_BACKQUOTE:
+		pass
+	if event.type == K_BACKSLASH:
+		pass
+	if event.type == K_BACKSPACE:
+		pass
+	if event.type == K_BREAK:
+		pass
+	if event.type == K_CAPSLOCK:
+		pass
+	if event.type == K_CARET:
+		pass
+	if event.type == K_CLEAR:
+		pass
+	if event.type == K_COLON:
+		pass
+	if event.type == K_COMMA:
+		pass
+	if event.type == K_DELETE:
+		pass
+	if event.type == K_DOLLAR:
+		pass
+	if event.type == K_DOWN:
+		pass
+	if event.type == K_END:
+		pass
+	if event.type == K_EQUALS:
+		pass
+	if event.type == K_ESCAPE:
+		pass
+	if event.type == K_EURO:
+		pass
+	if event.type == K_EXCLAIM:
+		pass
+	if event.type == K_F1:
+		pass
+	if event.type == K_F10:
+		pass
+	if event.type == K_F11:
+		pass
+	if event.type == K_F12:
+		pass
+	if event.type == K_F13:
+		pass
+	if event.type == K_F14:
+		pass
+	if event.type == K_F15:
+		pass
+	if event.type == K_F2:
+		pass
+	if event.type == K_F3:
+		pass
+	if event.type == K_F4:
+		pass
+	if event.type == K_F5:
+		pass
+	if event.type == K_F6:
+		pass
+	if event.type == K_F7:
+		pass
+	if event.type == K_F8:
+		pass
+	if event.type == K_F9:
+		pass
+	if event.type == K_FIRST:
+		pass
+	if event.type == K_GREATER:
+		pass
+	if event.type == K_HASH:
+		pass
+	if event.type == K_HELP:
+		pass
+	if event.type == K_HOME:
+		pass
+	if event.type == K_INSERT:
+		pass
+	if event.type == K_KP0:
+		pass
+	if event.type == K_KP1:
+		pass
+	if event.type == K_KP2:
+		pass
+	if event.type == K_KP3:
+		pass
+	if event.type == K_KP4:
+		pass
+	if event.type == K_KP5:
+		pass
+	if event.type == K_KP6:
+		pass
+	if event.type == K_KP7:
+		pass
+	if event.type == K_KP8:
+		pass
+	if event.type == K_KP9:
+		pass
+	if event.type == K_KP_DIVIDE:
+		pass
+	if event.type == K_KP_ENTER:
+		pass
+	if event.type == K_KP_EQUALS:
+		pass
+	if event.type == K_KP_MINUS:
+		pass
+	if event.type == K_KP_MULTIPLY:
+		pass
+	if event.type == K_KP_PERIOD:
+		pass
+	if event.type == K_KP_PLUS:
+		pass
+	if event.type == K_LALT:
+		pass
+	if event.type == K_LAST:
+		pass
+	if event.type == K_LCTRL:
+		pass
+	if event.type == K_LEFT:
+		pass
+	if event.type == K_LEFTBRACKET:
+		pass
+	if event.type == K_LEFTPAREN:
+		pass
+	if event.type == K_LESS:
+		pass
+	if event.type == K_LMETA:
+		pass
+	if event.type == K_LSHIFT:
+		pass
+	if event.type == K_LSUPER:
+		pass
+	if event.type == K_MENU:
+		pass
+	if event.type == K_MINUS:
+		pass
+	if event.type == K_MODE:
+		pass
+	if event.type == K_NUMLOCK:
+		pass
+	if event.type == K_PAGEDOWN:
+		pass
+	if event.type == K_PAGEUP:
+		pass
+	if event.type == K_PAUSE:
+		pass
+	if event.type == K_PERIOD:
+		pass
+	if event.type == K_PLUS:
+		pass
+	if event.type == K_POWER:
+		pass
+	if event.type == K_PRINT:
+		pass
+	if event.type == K_QUESTION:
+		pass
+	if event.type == K_QUOTE:
+		pass
+	if event.type == K_QUOTEDBL:
+		pass
+	if event.type == K_RALT:
+		pass
+	if event.type == K_RCTRL:
+		pass
+	if event.type == K_RETURN:
+		pass
+	if event.type == K_RIGHT:
+		pass
+	if event.type == K_RIGHTBRACKET:
+		pass
+	if event.type == K_RIGHTPAREN:
+		pass
+	if event.type == K_RMETA:
+		pass
+	if event.type == K_RSHIFT:
+		pass
+	if event.type == K_RSUPER:
+		pass
+	if event.type == K_SCROLLOCK:
+		pass
+	if event.type == K_SEMICOLON:
+		pass
+	if event.type == K_SLASH:
+		pass
+	if event.type == K_SPACE:
+		pass
+	if event.type == K_SYSREQ:
+		pass
+	if event.type == K_TAB:
+		pass
+	if event.type == K_UNDERSCORE:
+		pass
+	if event.type == K_UNKNOWN:
+		pass
+	if event.type == K_UP:
+		pass
+	if event.type == K_a:
+		pass
+	if event.type == K_b:
+		pass
+	if event.type == K_c:
+		pass
+	if event.type == K_d:
+		pass
+	if event.type == K_e:
+		pass
+	if event.type == K_f:
+		pass
+	if event.type == K_g:
+		pass
+	if event.type == K_h:
+		pass
+	if event.type == K_i:
+		pass
+	if event.type == K_j:
+		pass
+	if event.type == K_k:
+		pass
+	if event.type == K_l:
+		pass
+	if event.type == K_m:
+		pass
+	if event.type == K_n:
+		pass
+	if event.type == K_o:
+		pass
+	if event.type == K_p:
+		pass
+	if event.type == K_q:
+		pass
+	if event.type == K_r:
+		pass
+	if event.type == K_s:
+		pass
+	if event.type == K_t:
+		pass
+	if event.type == K_u:
+		pass
+	if event.type == K_v:
+		pass
+	if event.type == K_w:
+		pass
+	if event.type == K_x:
+		pass
+	if event.type == K_y:
+		pass
+	if event.type == K_z:
+		pass
+
+
+def pygameKeyboardRule_KEYUP():
+        
+	if event.type == K_0:
+		pass
+	if event.type == K_1:
+		pass
+	if event.type == K_2:
+		pass
+	if event.type == K_3:
+		pass
+	if event.type == K_4:
+		pass
+	if event.type == K_5:
+		pass
+	if event.type == K_6:
+		pass
+	if event.type == K_7:
+		pass
+	if event.type == K_8:
+		pass
+	if event.type == K_9:
+		pass
+	if event.type == K_AMPERSAND:
+		pass
+	if event.type == K_ASTERISK:
+		pass
+	if event.type == K_AT:
+		pass
+	if event.type == K_BACKQUOTE:
+		pass
+	if event.type == K_BACKSLASH:
+		pass
+	if event.type == K_BACKSPACE:
+		pass
+	if event.type == K_BREAK:
+		pass
+	if event.type == K_CAPSLOCK:
+		pass
+	if event.type == K_CARET:
+		pass
+	if event.type == K_CLEAR:
+		pass
+	if event.type == K_COLON:
+		pass
+	if event.type == K_COMMA:
+		pass
+	if event.type == K_DELETE:
+		pass
+	if event.type == K_DOLLAR:
+		pass
+	if event.type == K_DOWN:
+		pass
+	if event.type == K_END:
+		pass
+	if event.type == K_EQUALS:
+		pass
+	if event.type == K_ESCAPE:
+		pass
+	if event.type == K_EURO:
+		pass
+	if event.type == K_EXCLAIM:
+		pass
+	if event.type == K_F1:
+		pass
+	if event.type == K_F10:
+		pass
+	if event.type == K_F11:
+		pass
+	if event.type == K_F12:
+		pass
+	if event.type == K_F13:
+		pass
+	if event.type == K_F14:
+		pass
+	if event.type == K_F15:
+		pass
+	if event.type == K_F2:
+		pass
+	if event.type == K_F3:
+		pass
+	if event.type == K_F4:
+		pass
+	if event.type == K_F5:
+		pass
+	if event.type == K_F6:
+		pass
+	if event.type == K_F7:
+		pass
+	if event.type == K_F8:
+		pass
+	if event.type == K_F9:
+		pass
+	if event.type == K_FIRST:
+		pass
+	if event.type == K_GREATER:
+		pass
+	if event.type == K_HASH:
+		pass
+	if event.type == K_HELP:
+		pass
+	if event.type == K_HOME:
+		pass
+	if event.type == K_INSERT:
+		pass
+	if event.type == K_KP0:
+		pass
+	if event.type == K_KP1:
+		pass
+	if event.type == K_KP2:
+		pass
+	if event.type == K_KP3:
+		pass
+	if event.type == K_KP4:
+		pass
+	if event.type == K_KP5:
+		pass
+	if event.type == K_KP6:
+		pass
+	if event.type == K_KP7:
+		pass
+	if event.type == K_KP8:
+		pass
+	if event.type == K_KP9:
+		pass
+	if event.type == K_KP_DIVIDE:
+		pass
+	if event.type == K_KP_ENTER:
+		pass
+	if event.type == K_KP_EQUALS:
+		pass
+	if event.type == K_KP_MINUS:
+		pass
+	if event.type == K_KP_MULTIPLY:
+		pass
+	if event.type == K_KP_PERIOD:
+		pass
+	if event.type == K_KP_PLUS:
+		pass
+	if event.type == K_LALT:
+		pass
+	if event.type == K_LAST:
+		pass
+	if event.type == K_LCTRL:
+		pass
+	if event.type == K_LEFT:
+		pass
+	if event.type == K_LEFTBRACKET:
+		pass
+	if event.type == K_LEFTPAREN:
+		pass
+	if event.type == K_LESS:
+		pass
+	if event.type == K_LMETA:
+		pass
+	if event.type == K_LSHIFT:
+		pass
+	if event.type == K_LSUPER:
+		pass
+	if event.type == K_MENU:
+		pass
+	if event.type == K_MINUS:
+		pass
+	if event.type == K_MODE:
+		pass
+	if event.type == K_NUMLOCK:
+		pass
+	if event.type == K_PAGEDOWN:
+		pass
+	if event.type == K_PAGEUP:
+		pass
+	if event.type == K_PAUSE:
+		pass
+	if event.type == K_PERIOD:
+		pass
+	if event.type == K_PLUS:
+		pass
+	if event.type == K_POWER:
+		pass
+	if event.type == K_PRINT:
+		pass
+	if event.type == K_QUESTION:
+		pass
+	if event.type == K_QUOTE:
+		pass
+	if event.type == K_QUOTEDBL:
+		pass
+	if event.type == K_RALT:
+		pass
+	if event.type == K_RCTRL:
+		pass
+	if event.type == K_RETURN:
+		pass
+	if event.type == K_RIGHT:
+		pass
+	if event.type == K_RIGHTBRACKET:
+		pass
+	if event.type == K_RIGHTPAREN:
+		pass
+	if event.type == K_RMETA:
+		pass
+	if event.type == K_RSHIFT:
+		pass
+	if event.type == K_RSUPER:
+		pass
+	if event.type == K_SCROLLOCK:
+		pass
+	if event.type == K_SEMICOLON:
+		pass
+	if event.type == K_SLASH:
+		pass
+	if event.type == K_SPACE:
+		pass
+	if event.type == K_SYSREQ:
+		pass
+	if event.type == K_TAB:
+		pass
+	if event.type == K_UNDERSCORE:
+		pass
+	if event.type == K_UNKNOWN:
+		pass
+	if event.type == K_UP:
+		pass
+	if event.type == K_a:
+		pass
+	if event.type == K_b:
+		pass
+	if event.type == K_c:
+		pass
+	if event.type == K_d:
+		pass
+	if event.type == K_e:
+		pass
+	if event.type == K_f:
+		pass
+	if event.type == K_g:
+		pass
+	if event.type == K_h:
+		pass
+	if event.type == K_i:
+		pass
+	if event.type == K_j:
+		pass
+	if event.type == K_k:
+		pass
+	if event.type == K_l:
+		pass
+	if event.type == K_m:
+		pass
+	if event.type == K_n:
+		pass
+	if event.type == K_o:
+		pass
+	if event.type == K_p:
+		pass
+	if event.type == K_q:
+		pass
+	if event.type == K_r:
+		pass
+	if event.type == K_s:
+		pass
+	if event.type == K_t:
+		pass
+	if event.type == K_u:
+		pass
+	if event.type == K_v:
+		pass
+	if event.type == K_w:
+		pass
+	if event.type == K_x:
+		pass
+	if event.type == K_y:
+		pass
+	if event.type == K_z:
+		pass
+
